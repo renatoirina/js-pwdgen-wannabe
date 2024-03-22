d6c15577f7a6b2ec5185043d756b6aeb1be4df48
renatoirina <irinarenato793@gmail.com>
2024-03-22 14:57:56 +0100

    feat: add console logs of acquisited datas

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
 // ACQUISIZIONE DATI
 var name = prompt("Inserisci il tuo nome: ");
+console.log("Nome inserito: ", name);
 var surname = prompt("Inserisci il tuo cognome: ");
+console.log("Cognome inserito: ", surname);
 var favouriteColor = prompt("Inserisci il tuo colore preferito: ");
+console.log("Colore preferito inserito: ", favouriteColor);
 
 // GENERAZIONE NUMERO CASUALE
 
