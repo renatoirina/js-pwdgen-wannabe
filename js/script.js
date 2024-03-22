b98ccb9025845f5216dd92f771f94b84cc099e83
renatoirina <irinarenato793@gmail.com>
2024-03-22 14:52:53 +0100

    feat: implemented the acquisition of datas

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// ACQUISIZIONE DATI
+var name = prompt("Inserisci il tuo nome: ");
+var surname = prompt("Inserisci il tuo cognome: ");
+var favouriteColor = prompt("Inserisci il tuo colore preferito: ");
+
+// GENERAZIONE NUMERO CASUALE
+
+
+// OPERAZIONI LOGICHE
+
+
+// VISUALIZZAZIONE
